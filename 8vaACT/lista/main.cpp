8db58983a333688fb3a4c8fcf23afc2661502009
JesusSamano <113057832+JesusSamano@users.noreply.github.com>
2025-10-13 23:17:05 -0600

    Add files via upload

---
+++ 8vaACT/lista/main.cpp
@@ -0,0 +1,117 @@
+#include <iostream>
+
+template<class T>
+class LSLSE;
+
+template<class T>
+class node{
+private:
+    T data;
+    node<T>* sig;
+public:
+    node():sig(nullptr){};
+    friend class LSLSE<T>;
+};
+
+template<class T>
+class LSLSE{
+private:
+    node<T>* lista;
+public:
+    LSLSE():lista(nullptr){};
+    bool vacia()const;
+    node<T>* ultimo()const;
+    node<T>* primero()const;
+    node<T>* anterior(node<T>* pos)const;
+    void insertar(node<T>* pos, T elem);
+    bool eliminar(node<T>* pos);
+    void imprimir()const;
+};
+
+template<class T>
+void LSLSE<T>::imprimir()const{
+    node<T>* aux=lista;
+    while(aux!=nullptr){
+        std::cout<<aux->data<<"-> ";
+        aux=aux->sig;
+    }
+}
+
+template<class T>
+bool LSLSE<T>::eliminar(node<T>* pos){
+    if(vacia() || pos==nullptr){
+        return false;
+    }
+    if(pos==lista){
+        lista=lista->sig;
+    }
+    else{
+        anterior(pos)->sig=pos->sig;
+    }
+    delete pos;
+    return true;
+}
+
+template<class T>
+void LSLSE<T>::insertar(node<T>* pos, T elem){
+    node<T>* aux= new node<T>;
+    aux->data=elem;
+    if(pos==nullptr){
+        aux->sig=lista;
+        lista=aux;
+    }
+    else{
+        aux->sig=pos->sig;
+        pos->sig=aux;
+    }
+}
+
+template<class T>
+node<T>* LSLSE<T>::anterior(node<T>* pos)const{
+    if(vacia() || pos==nullptr){
+        return nullptr;
+    }
+    node<T>* aux=lista;
+    while(aux!=nullptr && aux->sig!=pos){
+        aux=aux->sig;
+    }
+    return aux;
+}
+
+template<class T>
+node<T>* LSLSE<T>::primero()const{
+    if(vacia()){
+        return nullptr;
+    }
+    return lista;
+}
+
+
+template<class T>
+node<T>* LSLSE<T>::ultimo()const{
+    if(vacia()){
+        return nullptr;
+    }
+    node<T>* aux=lista;
+    while(aux->sig!=nullptr){
+        aux=aux->sig;
+    }
+    return aux;
+}
+
+template<class T>
+bool LSLSE<T>::vacia()const{
+    if(lista==nullptr)
+        return true;
+    return false;
+}
+
+int main()
+{
+    LSLSE<float> milista;
+    for(int i=1; i<=100; i++){
+        milista.insertar(milista.ultimo(), float(i)/100);
+    }
+    milista.imprimir();
+    return 0;
+}
